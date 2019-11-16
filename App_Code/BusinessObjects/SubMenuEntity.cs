5d25d5cc0d22f95034611b2608c7152cbb4a30f9
surekhatelidevara <36558329+surekhatelidevara@users.noreply.github.com>
2019-11-16 11:50:59 +0530

    SubMenuEntity

---
+++ App_Code/BusinessObjects/SubMenuEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EntityManager
+{
+    public class SubMenuEntity
+    {
+        public int SubMenuID { get; set; }
+        public int MainMenuID { get; set; }
+        public string SubMenuName { get; set; }
+        public int Status { get; set; }
+    }
+}
