6e425ea0d48ed53b8bfe495a7142ea88113e0d55
tejaswiindukuri <tejaswi.indukuri@dinoosys.com>
2019-11-16 12:40:03 +0530

    Create Mainmenuentity
     Mainmenuentity.cs

---
+++ App_Code/BusinessObjects/MainMenuEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for MainMenuEntity
+/// </summary>
+public class MainMenuEntity
+{
+    public int MainMenuID { get; set; }
+    public string MenuName { get; set; }
+    public string Status { get; set; }
+}
