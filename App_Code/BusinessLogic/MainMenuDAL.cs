1f11f6a6fda17519dcf0e5d188e2a0b35308558f
tejaswiindukuri <tejaswi.indukuri@dinoosys.com>
2019-11-16 12:18:26 +0530

    DAL
    DAL classes

---
+++ App_Code/BusinessLogic/MainMenuDAL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+using System.Data;
+using DataManager;
+
+/// <summary>
+/// Summary description for MainMenuDAL
+/// </summary>
+public class MainMenuDAL
+{
+    DataUtilities objUtilities = new DataUtilities();
+	public int InsertMainMenu(MainMenuEntity ObjMainMenuEntity,char Operation)
+    {
+        Hashtable ht = new Hashtable();
+        ht.Add("@MenuName", ObjMainMenuEntity.MenuName);
+        ht.Add("@Status", ObjMainMenuEntity.Status);
+        ht.Add("@Operation", Operation);
+        return objUtilities.ExecuteNonQuery("InsertMainMenuDetails", ht);
+
+    }
+}
